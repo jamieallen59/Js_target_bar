d064c106506c6f66b9f1bbcb9249bfd39b3fa101
jamieallen59 <jamieallen59@gmail.com>
2014-08-31 21:12:47 +0100

    Added progress percentage

--- spec/TargetBarSpec.js
+++ spec/TargetBarSpec.js
@@ -3,18 +3,70 @@ describe("TargetBar", function() {
 
 
   beforeEach(function() {
-    targetbar = new TargetBar();
+    targetbar = new TargetBar(0, 100);
 
   });
 
-  it("should begin at start value or default", function() {
+  it("should begin at start value given, or default to 0", function() {
     expect(targetbar.amount).toEqual (0);
   });
 
-  describe("increasing the money raised", function() {
+  describe("Adding to the money raised", function() {
     it("increases if < the target amount", function() {
       targetbar.addMoney(1);
       expect(targetbar.amount).toEqual (1);
+    });
+
+    it("does not increase if the target amount has been raised", function(){
+      targetbar.amount = 100;
+      targetbar.addMoney(1);
+      expect(targetbar.amount).toEqual(100);
+    });
+  });
+
+  describe("Removing money", function() {
+    it("does not decrease if <= 0", function(){
+      targetbar.subtractMoney(1);
+      expect(targetbar.amount).toEqual(0);
+    });
+  });
+
+  describe("The target amount", function(){
+    it("can be adjusted by the user", function(){
+      targetbar.adjustTarget(100);
+      expect(targetbar.targetAmount).toEqual(200);
+    });
+  });
+
+
+  describe("Donation progress", function(){
+    it("displays the donation progress as a %", function(){
+      targetbar.amount = 40;
+      expect(targetbar.percentage()).toEqual("40%");
     })
+
+    it("needs encouragement if < 30% of target raised", function(){
+      targetbar.amount = 29;
+      expect(targetbar.progress()).toEqual("Struggling");
+    });
+
+    it("is doing ok if between 30% and 65%", function(){
+      targetbar.amount = 56;
+      expect(targetbar.progress()).toEqual("Steady");
+    });
+
+    it("is doing great if > 75% of target raised", function(){
+      targetbar.amount = 82;
+      expect(targetbar.progress()).toEqual("Great");
+    });
   });
 });
+
+  
+
+
+
+
+
+
+
--- src/TargetBar.js
+++ src/TargetBar.js
@@ -1,7 +1,29 @@
 function TargetBar(moneyRaised, targetAmount) {
 	this.amount = moneyRaised || 0;
+	this.targetAmount = targetAmount || 500;
 }
 
 TargetBar.prototype.addMoney = function(moneyDonated) {
-	this.amount += moneyDonated;
+	if(this.amount + moneyDonated < this.targetAmount) {
+		this.amount += moneyDonated;
+	}
+};
+
+TargetBar.prototype.subtractMoney = function(moneyRemoved) {
+	if(this.amount - moneyRemoved > 0) {
+		this.amount -= moneyRemoved;
+	}
+};
+
+TargetBar.prototype.adjustTarget = function(targetAddition) {
+	this.targetAmount += targetAddition;
+};
+
+TargetBar.prototype.percentage = function() {
+	var progress = (this.amount / 100) * this.targetAmount;
+	return progress.toString() + "%"
+};
+
+TargetBar.prototype.progress = function() {
+	
 };
